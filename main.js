e3b8a2fb5ea639ff4c7ba7b8983fa9bf20f4438e
Olivia Hallenberg <olivia.hallenberg@outlook.com>
2025-02-08 14:24:40 +0200

    Add react project

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const {app, BrowserWindow} = require('electron'); 
-const url = require('url');
-const path = require('path')
+const path = require('path');
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Electron',
-        width: 1000,
-        height: 600
+        width: 600,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            allowFileAccess: true 
+        }
+       
     });
 
-    const starturl = url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file'
-    });
+    mainWindow.webContents.openDevTools();
 
-    mainWindow.loadURL(starturl);
+    mainWindow.loadFile(path.join(__dirname,'viteandelectron', 'dist', 'index.html'));
 }
 
 app.whenReady().then(createMainWindow);
+
+
